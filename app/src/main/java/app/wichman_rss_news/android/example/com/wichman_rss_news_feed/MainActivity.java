06bf4d107822576a1ae92897fab6100c922ce6a4
Joseph <josephkwichman@gmail.com>
2015-04-04 15:51:04 -0500

    Initial Commit.

---
+++ app/src/main/java/app/wichman_rss_news/android/example/com/wichman_rss_news_feed/MainActivity.java
@@ -0,0 +1,92 @@
+package app.wichman_rss_news.android.example.com.wichman_rss_news_feed;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+
+    /**
+     * Called when the activity is first created.
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the ListView resource.
+        ListView mainListView = (ListView) findViewById(R.id.myMainListView);
+
+        // Create and populate a List of RSS List items.
+
+        String[] rss_feeds_items = getResources().getStringArray(R.array.rss_feeds_items);
+
+        // Create ArrayAdapter using the RSS Feeds Items list.
+        ArrayAdapter<?> listAdapter = new ArrayAdapter<>(this, R.layout.myrsslistitem, rss_feeds_items);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter(listAdapter);
+
+        /// listening to single list item on click
+
+        // mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // String List_items = ((TextView) view).getText().toString();
+
+                String url = "";
+                /// selected item
+                switch (position) {
+                    case 0:
+                        url = "http://feeds.reuters.com/reuters/topNews";
+                        break;
+                    case 1:
+                        url = "http://feeds.reuters.com/reuters/sportsNews";
+                        break;
+                    case 2:
+                        url = "http://rss.cnn.com/rss/cnn_topstories.rss";
+                        break;
+                    case 3:
+                        url = "http://feeds.foxnews.com/foxnews/sports";
+                        break;
+                    case 4:
+                        url = "http://www.msn.com/en-us/weather";
+                        break;
+                    default:
+                        url = "http://www.google.com";
+                        break;
+                }
+
+                /// Launching new Activity on selecting single List Item
+
+                 Intent i = new Intent(getApplicationContext(), RSSFeedActivity.class);
+                /// sending data to new activity
+//                 i.putExtra("url", url);
+                try {
+                   // Intent i = new Intent(getApplicationContext(), RSSFeedActivity.class);
+                    i.putExtra("url", url);
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/app/wichman_rss_news/android/example/com/wichman_rss_news_feed/RSSFeedActivity.java
@@ -0,0 +1,64 @@
+package app.wichman_rss_news.android.example.com.wichman_rss_news_feed;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class RSSFeedActivity extends Activity {
+
+    private WebView webview;
+    protected int wvCount;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+
+        wvCount = 0;
+
+          Intent i = getIntent();
+          Bundle b = i.getExtras();
+          String strUrl = (String) b.get("url");
+
+
+        Uri uri = Uri.parse(strUrl);
+//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//        startActivity(intent);
+
+        webview = (WebView) findViewById(R.id.rss_web_view);
+        webview.getSettings().setJavaScriptEnabled(true);
+        // Simplest usage: note that an exception will NOT be thrown
+        // if there is an error loading this page (see below).
+        webview.loadUrl(String.valueOf(uri));
+        WebViewClient client = new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                super.shouldOverrideUrlLoading(view, url);
+                view.loadUrl(url);
+                wvCount++;
+                return true;
+            }
+        };
+
+        webview.setWebViewClient(client);
+//        setContentView(webview);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wvCount == 0) {
+            super.onBackPressed();
+        } else {
+            if (webview.canGoBack()) {
+                webview.goBack();
+                wvCount--;
+            } else {
+                super.onBackPressed();
+            }
+        }
+    }
+}
